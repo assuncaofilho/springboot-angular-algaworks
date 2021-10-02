b8770308301eed0d14d0c51743113394780ce492
Marco Assunção <assuncaofilho@gmail.com>
2021-10-02 00:23:56 -0300

    Usando eventos para adicionar header Location

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 
@@ -26,6 +27,9 @@ public class PessoaResource {
 	
 	@Autowired
 	PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Pessoa> listar(){
@@ -38,17 +42,11 @@ public class PessoaResource {
 	//@ResponseStatus(HttpStatus.CREATED) é dispensável já que na resposta já estou dizendo o status.
 	//@Valid -> validação do Modelo pelo Spring; @RequestBody -> No corpo da requisição teremos um objeto Pessoa
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa Pessoa, HttpServletResponse response) {
-		Pessoa PessoaSalva = pessoaRepository.save(Pessoa);
+		Pessoa pessoaSalva = pessoaRepository.save(Pessoa);
 		
-		/*{codigo} pode ser usado livremente e o Spring o detecta como atributo de Pessoa, pois 
-		 * ao injetarmos pessoaRepository a mesma conhece a impl de JpaRepository<Pessoa,Long> ?*/
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}") // {codigo} é variável (Expression Language)
-		.buildAndExpand(PessoaSalva.getCodigo()).toUri();
-		
-		response.setHeader("Locations", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(PessoaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
